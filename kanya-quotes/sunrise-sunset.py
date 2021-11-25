792d82870823e0b3f825b8e71bb06f4205724352
Rabs <karkirabu@gmail.com>
2021-11-26 00:49:42 +0545

    sunirise-sunset

---
+++ kanya-quotes/sunrise-sunset.py
@@ -0,0 +1,21 @@
+import requests
+from datetime import datetime, time
+
+MY_LAT=28.3949
+My_LON=84.1240
+
+parameters={
+    "lat":MY_LAT,
+    "lon":My_LON,
+    "formatted":0
+}
+response=requests.get(url="https://api.sunrise-sunset.org/json",params=parameters)
+response.raise_for_status()
+data=response.json()
+sunrise=data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset=data["results"]["sunset"].split("T")[1].split(":")[0]
+print(sunrise)
+print(sunset)
+
+time_now=datetime.now()
+print(time_now.hour)
